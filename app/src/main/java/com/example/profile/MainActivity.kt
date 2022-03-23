9773d08b05fd98579a83c3402fae643eea344828
djdenielb <luisdmateos@gmail.com>
2022-03-23 12:40:27 -0600

    vieBinding habilitado en gradle y en main activity Creacion de la funcion para asignar los valores a los elementos graficos parametros de la funcion seran valores por default en los elementos graficos asignar valores por medio de binding

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.profile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateUI()
+    }
+
+    private fun updateUI(name: String = "Luis Daniel Mateos",
+                         email: String = "luisdmateos@gmail.com",
+                         website: String = "www.google.com",
+                         phone: String = "5540374031") {
+    binding.tvName.text = name
+    binding.tvEmail.text = email
+    binding.tvWebSite.text = website
+    binding.tvPhone.text = phone
+
     }
 }
