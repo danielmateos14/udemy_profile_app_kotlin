3c7c2008a5d1d6fa909538cc4d5f183b5a6c24cb
djdenielb <luisdmateos@gmail.com>
2022-03-23 13:05:00 -0600

    Creacion de variables con coordenadas en el mapa, latitud y longitud creacion del metodo onclicklistener para mostrar las variables que contienen las coordenadas
    cambio de tamaño de texto en el campo ubicacion en el xml

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -7,22 +7,35 @@ import com.example.profile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var lat = 0.0
+    private var long = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         updateUI()
+
+        binding.tvLocation.setOnClickListener {
+            binding.tvLocation.text = "Latitud: $lat \nLongitud: $long"
+        }
+
     }
 
-    private fun updateUI(name: String = "Luis Daniel Mateos",
-                         email: String = "luisdmateos@gmail.com",
-                         website: String = "www.google.com",
-                         phone: String = "5540374031") {
-    binding.tvName.text = name
-    binding.tvEmail.text = email
-    binding.tvWebSite.text = website
-    binding.tvPhone.text = phone
+    private fun updateUI(
+        name: String = "Luis Daniel Mateos",
+        email: String = "luisdmateos@gmail.com",
+        website: String = "www.google.com",
+        phone: String = "5540374031"
+    ) {
+        binding.tvName.text = name
+        binding.tvEmail.text = email
+        binding.tvWebSite.text = website
+        binding.tvPhone.text = phone
+
+        lat = 19.633397167523707
+        long = -99.12184445830947
 
     }
 }
